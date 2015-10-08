6f1921d4b8ebb9e86beef511ed976122e1821377
Filip-Konkowski <fkonkowski@gmail.com>
2015-10-08 17:24:15 +0200

    first commit of the project

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/main.css">
+    <title></title>
+</head>
+<body>
+
+
+
+
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/jquery_ready.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
+
